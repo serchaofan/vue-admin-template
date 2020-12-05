9e8883adf1d7c533940d23826fa236b6fb7d1984
serchaofan <624261309@qq.com>
2020-12-06 01:00:21 +0800

    删除所有用户相关功能

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,19 +1,11 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
-  </div>
+  <div class="dashboard-container" />
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Dashboard',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
-  }
+  name: 'Dashboard'
 }
 </script>
 
